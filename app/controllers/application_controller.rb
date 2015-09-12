51ba0260394c16fd1d73b9aee2ae16467bd6bd9a
adriano benin <adriano.benin@gmail.com>
2015-09-12 15:28:25 -0300

    add new relic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   include Concerns::AnalyticsHelpersHandler
   include Pundit
   if Rails.env.production?
-    #require "new_relic/agent/instrumentation/rails3/action_controller"
+    require "new_relic/agent/instrumentation/rails3/action_controller"
     include NewRelic::Agent::Instrumentation::ControllerInstrumentation
     include NewRelic::Agent::Instrumentation::Rails3::ActionController
   end
