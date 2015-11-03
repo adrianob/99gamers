f6105c929e0eaa8595d381b7123ec8d8ec5531ec
Adriano Benin <adriano.benin@gmail.com>
2015-11-03 13:11:24 -0200

    delegate expired? to project

--- app/models/contributed_project.rb
+++ app/models/contributed_project.rb
@@ -4,7 +4,7 @@ class ContributedProject < ActiveRecord::Base
   belongs_to :subscription
 
   scope :most_recent_first, ->{ order("online_date DESC, created_at DESC") }
-  delegate :most_recent_first, :decorate, :progress_bar, :progress, :display_pledged, :display_image, :funding_type, to: :project
+  delegate :most_recent_first, :decorate, :progress_bar, :progress, :display_pledged, :display_image, :funding_type, :expired?, to: :project
 
   def project
     Project.find self.id
