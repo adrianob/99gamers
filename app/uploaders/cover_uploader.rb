ce1c875a42279e8cdc8a4f59a8fea00deb0831fc
Adriano Benin <adriano.benin@gmail.com>
2015-12-20 18:24:08 -0200

    fix cover image

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -3,9 +3,15 @@
 class CoverUploader < ImageUploader
 
   version :base
+  version :project_cover
 
   version :base do
     process resize_to_fill: [1200,800]
+    process convert: :jpg
+  end
+
+  version :project_cover do
+    process resize_to_fit: [1200,1800]
     process convert: :jpg
   end
 
