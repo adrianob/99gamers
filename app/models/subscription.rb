372a531ed5544bfa86810205e3113fbb8f7f01f6
Adriano Benin <adriano.benin@gmail.com>
2016-02-15 14:09:41 -0200

    order sub by created_at

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,9 +8,9 @@ class Subscription < ActiveRecord::Base
   has_many :subscription_notifications
 
   #do not show canceled
-  scope :public_active, ->(){ where("subscriptions.state IN ('paid', 'pending_payment')").order('subscriptions.state') }
+  scope :public_active, ->(){ where("subscriptions.state IN ('paid', 'pending_payment')").order('subscriptions.state').order('subscriptions.created_at desc') }
 
-  scope :active, ->{ joins(:plan).where("(state in ('paid','pending_payment')) OR (state = 'canceled' AND ((SELECT created_at from subscription_notifications sn where sn.subscription_id = subscriptions.id order by created_at DESC LIMIT 1 ) + (interval '1 days' * plans.days)) > current_timestamp)").order('state') }
+  scope :active, ->{ joins(:plan).where("(state in ('paid','pending_payment')) OR (state = 'canceled' AND ((SELECT created_at from subscription_notifications sn where sn.subscription_id = subscriptions.id order by created_at DESC LIMIT 1 ) + (interval '1 days' * plans.days)) > current_timestamp)").order('state').order('subscriptions.created_at desc') }
   scope :paid, -> do
    where("subscriptions.gateway_data->>'status' IN ('paid')")
   end
--- app/models/subscription_detail.rb
+++ app/models/subscription_detail.rb
@@ -7,7 +7,7 @@ class SubscriptionDetail < ActiveRecord::Base
 
   delegate :pending_payment?, to: :subscription
 
-  scope :public_active, ->(){ joins(:subscription).where("subscriptions.state IN ('paid', 'pending_payment')").order('subscriptions.state') }
-  scope :active, ->{ joins(:plan).joins(:subscription).where("(subscriptions.state in ('paid','pending_payment')) OR (subscriptions.state = 'canceled' AND ((SELECT created_at from subscription_notifications sn where sn.subscription_id = subscription_details.id order by created_at DESC LIMIT 1 ) + (interval '1 days' * plans.days)) > current_timestamp)").order('subscriptions.state') }
+  scope :public_active, ->(){ joins(:subscription).where("subscriptions.state IN ('paid', 'pending_payment')").order('subscriptions.state').order('subscriptions.created_at desc') }
+  scope :active, ->{ joins(:plan).joins(:subscription).where("(subscriptions.state in ('paid','pending_payment')) OR (subscriptions.state = 'canceled' AND ((SELECT created_at from subscription_notifications sn where sn.subscription_id = subscription_details.id order by created_at DESC LIMIT 1 ) + (interval '1 days' * plans.days)) > current_timestamp)").order('subscriptions.state').order('subscriptions.created_at desc') }
 
 end
